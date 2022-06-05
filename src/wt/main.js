da1805b01344178de68167ac4739a5b41b3ea383
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 21:58:48 +0300

    feat: implement Fibonacci workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,29 @@
+import {Worker} from 'worker_threads';
+import {cpus} from "os";
+
 export const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+
+    const promise = (workerData) => () =>
+        new Promise((resolve) => {
+            new Worker(new URL('./worker.js', import.meta.url), {
+                workerData,
+            })
+                .on('message', (data) => {
+                    resolve({status: 'resolved', data: data});
+                })
+                .on('error', () => {
+                    resolve({status: 'error', data: null});
+                });
+        });
+
+    for (let n = 0; n < cpus().length; n++) {
+        promises.push(promise(n + 10)());
+    }
+
+    return Promise.all(promises);
 };
+
+performCalculations().then(output => {
+    console.log(output);
+});
