32b228b20155412423323ed643edb6861845c770
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 18:55:07 +0300

    streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import {open} from 'fs/promises';
+import {Transform} from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const writeable = (
+        await open(
+            new URL('./files/fileToWrite.txt', import.meta.url), 'w+'
+        )
+    ).createWriteStream();
+
+    const reverseStream = new Transform({
+        transform (data, encoding, callback) {
+            const reversedData = data.toString().split("").reverse().join("");
+            this.push(reversedData);
+            callback();
+        }
+    });
+
+    process.stdin.pipe(reverseStream).pipe(writeable);
 };
+
+transform();
