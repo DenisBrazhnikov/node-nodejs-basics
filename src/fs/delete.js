f6b0a0e94b9f1df29415fd5cf97ec09c5d059329
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 17:12:55 +0300

    fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import fs from 'fs/promises';
+
 export const remove = async () => {
-    // Write your code here 
+
+    try {
+        await fs.rm(new URL('./files/fileToRemove.txt', import.meta.url));
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            console.error(new Error('FS operation failed'));
+        }
+    }
 };
+
+remove();
