ffa6a547e4855bb60819e8bb858004a79697e4ff
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 18:15:09 +0300

    streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import {open} from 'fs/promises';
+
 export const write = async () => {
-    // Write your code here 
+    const writeable = (
+        await open(
+            new URL('./files/fileToWrite.txt', import.meta.url), 'w+'
+        )
+    ).createWriteStream();
+
+    process.stdin.on('data', (buff) => {
+        writeable.write(buff);
+    });
 };
+
+write();
