e3ff47e1c7371a1882145366347f4b353e1549bd
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 17:13:17 +0300

    fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 export const rename = async () => {
-    // Write your code here 
+    const basePath = path.dirname(fileURLToPath(import.meta.url));
+
+    try {
+        await fs.rename(
+            path.join(basePath, '/files', 'wrongFileName.txt'),
+            path.join(basePath, '/files', 'properFilename.md')
+        );
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            console.error(new Error('FS operation failed'));
+        }
+    }
 };
+
+rename();
