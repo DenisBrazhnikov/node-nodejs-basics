70f003f1072a3f0a681a290abb96d6377bd5e8f4
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 17:12:40 +0300

    fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import fs from 'fs/promises';
+
 export const create = async () => {
-    // Write your code here 
+    try {
+        await fs.writeFile(
+            new URL('./files/fresh.txt', import.meta.url),
+            'I am fresh and young',
+            {
+                flag: 'wx'
+            }
+        );
+    } catch (e) {
+        if (e.code === 'EEXIST') {
+            console.error(new Error('FS operation failed'));
+        }
+    }
 };
+
+create();
