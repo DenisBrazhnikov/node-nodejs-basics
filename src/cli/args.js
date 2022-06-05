a88cbeb1a579446e8a0e268f0ec40cc845594a7e
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 17:13:31 +0300

    cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
+import {argv} from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    const outputArray = [];
+
+    argv.slice(2).forEach((value, key) => {
+        outputArray.push(key + ' is ' + value);
+    })
+
+    console.log(outputArray.join(', '));
 };
+
+parseArgs();
