0e55895a4682f94456822c0fd0e0900156961da1
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-05 17:13:55 +0300

    hash:calc

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,12 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const hashSum = crypto.createHash('sha256');
+
+    hashSum.update(await fs.readFile(new URL('./files/fileToCalculateHashFor.txt', import.meta.url)));
+
+    console.log(hashSum.digest('hex'));
 };
+
+calculateHash();
